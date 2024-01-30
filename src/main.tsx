068b024d63b01c8126d23cc9e244c7eaa454b6d7
Gabriel Facco de Arruda <arrudagates@gmail.com>
2024-01-30 13:45:21 +0800

    Remove transfer route

--- src/main.tsx
+++ src/main.tsx
@@ -62,8 +62,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 
               <Route path="claim" element={<Claim />} />
 
-              <Route path="transfer" element={<Transfer />} />
-
               <Route path="staking" element={<Staking />} />
 
               <Route path="404" element={<NotFound />} />
