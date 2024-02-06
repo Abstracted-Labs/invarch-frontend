efa0ddd293a2b75de4c14dd20e8096970ff44de0
Yaki <shibatales@gmail.com>
2024-02-06 14:20:27 -0800

    Refactor staking component to update total claimed and unclaimed rewards

--- src/routes/staking.tsx
+++ src/routes/staking.tsx
@@ -464,7 +464,8 @@ const Staking = () => {
           return;
         }
 
-        if (initialUnclaimed.current !== null) {
+        if (initialUnclaimed.current !== null && initialUnclaimed.current > BigNumber(0)) {
+          console.log('got here:', initialUnclaimed.current.toString());
           setTotalClaimed(prevTotalClaimed => prevTotalClaimed.plus(initialUnclaimed.current || new BigNumber(0)));
         }
 
@@ -520,17 +521,14 @@ const Staking = () => {
     const rewardsClaimed = new BigNumber(
       rewardsUserClaimedQuery.data.stakers[0].totalRewards
     );
-    setTotalClaimed(rewardsClaimed);
 
     const totalUnclaimed = new BigNumber(
       rewardsUserClaimedQuery.data.stakers[0].totalUnclaimed
     );
-    setTotalUnclaimed(claimAllSuccess ? new BigNumber(0) : totalUnclaimed);
 
-    if (initialUnclaimed.current === null) {
-      initialUnclaimed.current = totalUnclaimed;
-    }
-  }, [selectedAccount, rewardsUserClaimedQuery.fetching, rewardsUserClaimedQuery.data, claimAllSuccess, refreshQuery]);
+    setTotalClaimed((prev) => claimAllSuccess ? prev.plus(totalUnclaimed) : rewardsClaimed);
+    setTotalUnclaimed(claimAllSuccess ? new BigNumber(0) : totalUnclaimed);
+  }, [selectedAccount, rewardsUserClaimedQuery.fetching, rewardsUserClaimedQuery.data, claimAllSuccess]);
 
   return (
     <div className="mx-auto w-full flex max-w-7xl flex-col justify-between p-4 sm:px-6 lg:px-8 mt-14 md:mt-0 gap-3">
